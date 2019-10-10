4af643232d4206332a408ed20aa68b09901abfab
Mothrakk <mothrakk1@gmail.com>
2019-10-11 02:19:29 +0300

    initial

---
+++ LarvaSharp/LarvaLibs/Utility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LarvaSharp.LarvaLibs
+{
+    static class Utility
+    {
+        public static string Timestamp()
+        {
+            return DateTime.Now.ToString("HH:mm:ss");
+        }
+    }
+}
---
+++ LarvaSharp/Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using LarvaSharp.LarvaLibs;
+
+namespace LarvaSharp {
+    class Program
+    {
+        static void Main()
+        {
+            int i = 0;
+            while (true)
+            {
+                while (!Console.KeyAvailable)
+                {
+                    Console.WriteLine(i++);
+                    Thread.Sleep(1000);
+                }
+                Console.Write('>');
+                Console.ReadLine();
+            }
+		}
+	}
+}

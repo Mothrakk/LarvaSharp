48c7a28a0036649184c2055614913f53885e2ae2
Mothrakk <mothrakk1@gmail.com>
2019-10-14 20:55:09 +0300

    Simplified PythonEvaluate

--- LarvaSharp/LarvaLibs/Commanding/Commands/PythonEvaluate.cs
+++ LarvaSharp/LarvaLibs/Commanding/Commands/PythonEvaluate.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using LarvaSharp.LarvaLibs.Commanding;
-using LarvaSharp.LarvaLibs.Managers;
+﻿using LarvaSharp.LarvaLibs.Managers;
 
 namespace LarvaSharp.LarvaLibs.Commanding.Commands
 {
@@ -15,13 +12,7 @@ namespace LarvaSharp.LarvaLibs.Commanding.Commands
         public void Run(string[] args, ManagerInfo managerInfo = null)
         {
             string c = string.Format("-c print({0})", string.Join(" ", args));
-            Process p = new Process();
-            p.StartInfo.FileName = "python.exe";
-            p.StartInfo.Arguments = c;
-            p.StartInfo.UseShellExecute = false;
-            p.Start();
-            p.WaitForExit();
-            p.Close();
+            Utility.RunProcess("python.exe", c);
         }
     }
 }
